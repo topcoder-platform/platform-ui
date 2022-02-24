28a7cfc8e793ae9e5f0b72eaa94c08eb406a4a9f
brooketopcoder <brooke.souza@topcoder.com>
2022-02-24 15:52:32 -0800

    PLAT-522 #comment logged in UI #time 45m

--- src/header/utilities/UtilitySelector/ProfileSelector/ProfileSeletor.tsx
+++ src/header/utilities/UtilitySelector/ProfileSelector/ProfileSeletor.tsx
@@ -4,19 +4,24 @@ import { FC } from 'react'
 import '../../../../lib/styles/index.scss'
 import { UiRoute } from '../../../../lib/urls'
 
+import avatarImage from './duck.png'
 import styles from './ProfileSelector.module.scss'
 
 const ProfileSelector: FC<{}> = () => {
 
     const routes: UiRoute = new UiRoute()
+
+    // TODO: get this from the authentication and user profile service, respectively
+    const isLoggedIn: boolean = true
+
+    const logIn: JSX.Element | undefined = isLoggedIn ? undefined : (<a href={routes.login} className='button'>Log In</a>)
+    const signUp: JSX.Element | undefined = isLoggedIn ? undefined : (<a href={routes.signup} className={classNames('button', 'allWhite')}>Sign Up</a>)
+    const avatar: JSX.Element | undefined = isLoggedIn ? (<img src={avatarImage} className={styles.avatar} />) : undefined
     return (
         <div className={styles['profile-selector']}>
-            <a href={routes.login} className='button'>
-                Log In
-            </a>
-            <a href={routes.signup} className={classNames('button', 'allWhite')}>
-                Sign Up
-            </a>
+            {logIn}
+            {signUp}
+            {avatar}
         </div>
     )
 }
