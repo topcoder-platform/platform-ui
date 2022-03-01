ed2447ef03da2880d297784b6dc27160d8651bfb
brooketopcoder <brooke.souza@topcoder.com>
2022-03-01 13:28:15 -0800

    PLAT-522 #comments convert App to functional component; clean-up #time 30m

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // TODO: import styles from './App.scss'
-import { Component } from 'react'
+import { Dispatch, FC, SetStateAction, useEffect, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import Header from './header/Header'
@@ -8,50 +8,41 @@ import Placeholder from './lib/placeholder/Placeholder'
 import { AuthenticationService } from './lib/services'
 import { UiRoute } from './lib/urls'
 
-class App extends Component<{}, AppState> {
+const App: FC<{}> = () => {
 
-    // TODO: determine the best way to inject these
-    // so we don't have to new it up every time.
-    // it's not a singleton, so this is good for now.
-    private readonly authenticationService: AuthenticationService = new AuthenticationService()
-    private readonly routes: UiRoute = new UiRoute()
+    // TODO: convert auth service to a provider
+    const authenticationService: AuthenticationService = new AuthenticationService()
 
-    constructor(props: {}) {
-        super(props)
-        this.state = {
-            auth: {},
-        }
-    }
+    const [appState, setAppState]: [AppState, Dispatch<SetStateAction<AppState>>] = useState<AppState>({
+        auth: {},
+    })
 
-    async componentDidMount(): Promise<void> {
+    useEffect(() => {
 
         // if we have already have the profile, don't do anything else
-        if (!!this.state.profile) {
-            return Promise.resolve()
+        if (!!appState?.profile) {
+            return
         }
 
-        // TODO: this seems awfully late in the lifecycle to set the auth data.
-        // it seems like there's got to be a way to make an async call before a component mounts
-
+        // TODO: move this to the provider
         // try to get a profile
-        const appState: AppState = await this.authenticationService.authenticate(this.state)
-        this.setState({
-            ...appState,
-        })
-    }
+        authenticationService.authenticate(appState)
+            .then(updatedAppState => setAppState(updatedAppState))
+    }, [])
 
-    render(): JSX.Element {
-        return (
-            <>
-                <Header initialized={!!this.state.auth.initialized} profile={this.state.profile} />
-                <Routes>
-                    <Route path={this.routes.designLibFonts} element={<Placeholder title='Design Library Fonts' />} />
-                    <Route path={this.routes.designLib} element={<Placeholder title='Design Library' />} />
-                    <Route path={this.routes.home} element={<Placeholder title='Platform UI Home' />} />
-                </Routes>
-            </>
-        )
-    }
+    // TODO: make routes configurable
+    const routes: UiRoute = new UiRoute()
+
+    return (
+        <>
+            <Header initialized={!!appState.auth.initialized} profile={appState.profile} />
+            <Routes>
+                <Route path={routes.designLibFonts} element={<Placeholder title='Design Library Fonts' />} />
+                <Route path={routes.designLib} element={<Placeholder title='Design Library' />} />
+                <Route path={routes.home} element={<Placeholder title='Platform UI Home' />} />
+            </Routes>
+        </>
+    )
 }
 
 export default App
