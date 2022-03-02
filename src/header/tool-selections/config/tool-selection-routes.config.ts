20aea4979a6d84d2a62e5749aafff5bbb3200aa7
brooketopcoder <brooke.souza@topcoder.com>
2022-03-02 10:05:09 -0800

    PLAT-778 #comment add comment #time 5m

--- src/header/tool-selections/config/tool-selection-routes.config.ts
+++ src/header/tool-selections/config/tool-selection-routes.config.ts
@@ -3,6 +3,7 @@ import { ToolSelectorProps } from '../models'
 
 const routes: UiRoute = new UiRoute()
 
+/* TODO: do not hard-code this here and have each app "register" itself somehow */
 export const toolSelectors: Array<ToolSelectorProps> = [
     {
         route: routes.home,
