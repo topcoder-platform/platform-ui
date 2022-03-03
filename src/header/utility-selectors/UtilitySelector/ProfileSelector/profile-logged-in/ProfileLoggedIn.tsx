f32675ca6a1965a98272a4f4ea46aef475ddc9b1
brooketopcoder <brooke.souza@topcoder.com>
2022-03-03 12:18:20 -0800

    PLAT-537 #comment overlay for opened panel #time 15m

--- src/header/utility-selectors/UtilitySelector/ProfileSelector/profile-logged-in/ProfileLoggedIn.tsx
+++ src/header/utility-selectors/UtilitySelector/ProfileSelector/profile-logged-in/ProfileLoggedIn.tsx
@@ -3,6 +3,7 @@ import { FC, useContext, useState } from 'react'
 import { Avatar, ProfileContext, ProfileContextData } from '../../../../../lib'
 
 import { ProfilePanel } from './profile-panel'
+import styles from './ProfileLoggedIn.module.scss'
 
 const ProfileLoggedIn: FC<{}> = () => {
 
@@ -30,6 +31,14 @@ const ProfileLoggedIn: FC<{}> = () => {
                     handle={profile.handle}
                     photoUrl={profile.photoURL}
                 />
+                {profilePanelOpen && (
+                    <div className={styles.overlay}>
+                        {/* TODO: import svg from library */}
+                        <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                            <path d='M6 18L18 6M6 6L18 18' stroke='#137D60' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' />
+                        </svg>
+                    </div>
+                )}
             </a>
             {profilePanelOpen && <ProfilePanel />}
         </>
