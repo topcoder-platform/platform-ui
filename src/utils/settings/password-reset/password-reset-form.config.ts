00e2c85899892b86f7b2143c4379756bc778721c
brooketopcoder <brooke.souza@topcoder.com>
2022-03-18 14:18:02 -0700

    PLAT-907 #comment todos for password validators #time 5m

--- src/utils/settings/password-reset/password-reset-form.config.ts
+++ src/utils/settings/password-reset/password-reset-form.config.ts
@@ -16,6 +16,7 @@ export const passwordFormDef: FormDefinition = {
         type: 'password',
         validators: [
             validatorRequired,
+            // TODO: match validator
         ],
     },
     newPassword: {
@@ -27,6 +28,7 @@ export const passwordFormDef: FormDefinition = {
         type: 'password',
         validators: [
             validatorRequired,
+            // TODO: password validator
         ],
     },
     [PasswordFieldName.currentPassword]: {
