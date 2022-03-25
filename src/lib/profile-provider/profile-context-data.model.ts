19ca50de2411bf0fc762b7b7be206a6aeb9790b3
Adam Meikle <adam.meikle22@gmail.com>
2022-03-25 14:23:56 -0400

    reverted userprofile to detail type change

--- src/lib/profile-provider/profile-context-data.model.ts
+++ src/lib/profile-provider/profile-context-data.model.ts
@@ -1,9 +1,9 @@
 import { PasswordUpdateRequest } from './password-update-request.model'
-import { UserProfileDetail } from './user-profile-detail.model'
+import { UserProfile } from './user-profile.model'
 
 export interface ProfileContextData {
     initialized: boolean
-    profile?: UserProfileDetail
+    profile?: UserProfile
     updatePassword: (userId: number, request: PasswordUpdateRequest) => Promise<void>
     updateProfile: (updatedProfileContext: ProfileContextData) => Promise<void>
 }
