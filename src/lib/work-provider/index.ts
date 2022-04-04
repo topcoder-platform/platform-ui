2ebad6617bd5f9bb1da7598ca76596e75a1fa3a3
Adam Meikle <adam.meikle22@gmail.com>
2022-04-04 04:42:38 -0400

    missed file

--- src/lib/work-provider/index.ts
+++ src/lib/work-provider/index.ts
@@ -1,8 +1,6 @@
 export { workGetAsync as getWork } from './work-functions'
 export { WorkProvider } from './work.provider'
 export { workContext } from './work.context'
-/* eslint-disable */
 export type { WorkItem } from './work-functions'
 export type { WorkList } from './work-functions'
 export type { WorkContextData } from './work-context-data.model'
-/* eslint-enable */
