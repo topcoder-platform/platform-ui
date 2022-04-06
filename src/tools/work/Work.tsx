9e0ea5bba0a8a7f5192187d06597f2c18986e1bc
Adam Meikle <adam.meikle22@gmail.com>
2022-04-06 08:36:49 -0400

    updated css and added sort functionality (not real data)

--- src/tools/work/Work.tsx
+++ src/tools/work/Work.tsx
@@ -8,8 +8,8 @@ export const toolTitle: string = 'Work'
 
 const SelfService: FC<{}> = () => {
 
-    let workStatuses: Array<string> = ['DRAFT', 'ACTIVE', 'SUBMITTED', 'IN REVIEW', 'REDIRECTED', 'CANCELLED', 'COMPLETE', 'ALL'];
-    let workTypes: Array<string> = ['Website Design', 'Website Development', 'Data Exploration'];
+    const workStatuses: Array<string> = ['DRAFT', 'ACTIVE', 'SUBMITTED', 'IN REVIEW', 'REDIRECTED', 'CANCELLED', 'COMPLETE', 'ALL'];
+    const workTypes: Array<string> = ['Website Design', 'Website Development', 'Data Exploration'];
 
     const workItem: WorkItem = {
         challengeStatus: "DRAFT",
@@ -23,9 +23,27 @@ const SelfService: FC<{}> = () => {
         rating: 0,
         status: "DRAFT",
         workStatus: "DRAFT",
-    }
+    };
 
-    let showingStatus = 1;
+    var allWork: Array<WorkItem> = [workItem, workItem];
+
+    function isDraft(workItem: WorkItem) { return (workItem.rating === 0); }
+    function isActive(workItem: WorkItem) { return (workItem.rating === 1); }
+    function isSubmitted(workItem: WorkItem) { return (workItem.rating === 2); }
+    function isInReview(workItem: WorkItem) { return (workItem.rating === 3); }
+    function isRedirected(workItem: WorkItem) { return (workItem.rating === 4); }
+    function isCancelled(workItem: WorkItem) { return (workItem.rating === 5); }
+    function isComplete(workItem: WorkItem) { return (workItem.rating === 6); }
+
+    var draftWork = allWork.filter(isDraft);
+    var activeWork = allWork.filter(isActive);
+    var submittedWork = allWork.filter(isDraft);
+    var inReviewWork = allWork.filter(isDraft);
+    var redirectedWork = allWork.filter(isDraft);
+    var cancelledWork = allWork.filter(isDraft);
+    var completeWork = allWork.filter(isDraft);
+    var indexedWork: Array<Array<WorkItem>> = [draftWork, activeWork, submittedWork, inReviewWork, redirectedWork, cancelledWork, completeWork];
+    let showingStatus = 0;
     let workItemStatus = workItem.numOfRegistrants;
     let workItemType = workItem.rating;
 
@@ -37,15 +55,12 @@ const SelfService: FC<{}> = () => {
     const cancelledCount = 2;
     const allCount = 11;
 
-    const activeClicked = () => {
-        alert("show active");
-        workItem.name="blah";
+    const activeClicked = () => { alert("show active"); showingStatus = 1;
         //this.forceUpdate();
     }
-
-    const draftClicked = () => { alert("show draft"); }
-    const redirectedClicked = () => { alert("show redirected"); }
-    const doneClicked = () => { alert("show done"); }
+    const draftClicked = () => { alert(indexedWork[0].length); showingStatus = 0; }
+    const redirectedClicked = () => { alert("show redir"); showingStatus = 4; }
+    const doneClicked = () => { alert("show complete"); showingStatus = 6; }
     const readyClicked = () => { alert("show ready");}
     const cancelledClicked = () => { alert("show cancelled");}
     const allClicked = () => { alert("show all");}
@@ -87,7 +102,7 @@ const SelfService: FC<{}> = () => {
                                     <div className={styles["work-header-cell-active-text"]}>
                                         <p onClick={() => activeClicked()}>{workStatuses[1]}</p>
                                     </div>
-                                    {showingStatus == 1 &&
+                                    {showingStatus === 1 &&
                                         <div className={styles["work-header-visible-category"]} />
                                     }
                                     <div className={styles["work-header-cell-active-count-bg"]}>
@@ -205,81 +220,107 @@ const SelfService: FC<{}> = () => {
                 </colgroup>
                 <tbody>
                     <tr>
+                        <td/>
+                        <td/>
                         <td>
-                            {workItemType === 0 && <DesignIcon/>}
-                            {workItemType === 1 && <DevelopmentIcon/>}
-                            {workItemType === 2 && <DataScienceIcon/>}
+                            <div className={styles["work-sort-by-text"]}>STATUS</div>
                         </td>
                         <td>
-                            <div className={styles["work-cell-title-inner"]}>
-                                <div className={styles["work-title"]}>{workItem.name}</div>
-                                <br/>
-                                <div className={styles["work-description"]}>
-                                    A dog walking website that allows visitors to select dog walkers and
-                                    schedule dog walking appointments
-                                </div>
-                            </div>
+                            <div className={styles["work-sort-by-text"]}>TYPE</div>
                         </td>
                         <td>
-                            {workItemStatus === 0 &&
-                                <div className={styles["work-status-circle-draft"]}/>
-                            }
-                            {workItemStatus === 1 &&
-                                <div className={styles["work-status-circle-active"]}/>
-                            }
-                            {workItemStatus === 2 &&
-                                <div className={styles["work-status-circle-submitted"]}/>
-                            }
-                            {workItemStatus === 3 &&
-                                <div className={styles["work-status-circle-in-review"]}/>
-                            }
-                            {workItemStatus === 4 &&
-                                <div className={styles["work-status-circle-redirected"]}/>
-                            }
-                            {workItemStatus === 5 &&
-                                <div className={styles["work-status-circle-cancelled"]}/>
-                            }
-                            {workItemStatus === 6 &&
-                                <div className={styles["work-status-circle-complete"]}/>
-                            }
-                            <div className={styles["work-status-text"]}>
-                                {workStatuses[workItemStatus]}
-                            </div>
+                            <div className={styles["work-sort-by-text"]}>CREATED</div>
                         </td>
                         <td>
-                            <div className={styles["work-type-text"]}>
-                                {workTypes[workItemType]}
-                            </div>
+                            <div className={styles["work-sort-by-text"]}>SOLUTIONS READY</div>
                         </td>
                         <td>
-                            <div className={styles["work-created-text"]}>{workItem.created}</div>
+                            <div className={styles["work-sort-by-text"]}>COST</div>
                         </td>
                         <td>
-                            <div className={styles["work-solutions-text"]}>April 28, 2022</div>
+                            <div className={styles["work-sort-by-text"]}>MESSAGES</div>
                         </td>
-                        <td>
-                            <div className={styles["work-cost-text"]}>$12,000</div>
-                        </td>
-                        <td>
-                            {workItem.messagesHasNew &&
-                                <div className={styles["work-messages-chip-red"]}>
-                                    <div className={styles["work-messages-text"]}>
-                                        {workItem.messagesCount}
-                                    </div>
-                                </div>
-                            }
-                            {!workItem.messagesHasNew &&
-                                <div className={styles["work-messages-chip-gray"]}>
-                                    <div className={styles["work-messages-text"]}>
-                                        {workItem.messagesCount}
-                                    </div>
-                                </div>
-                            }
-                        </td>
-                        <td>
-                            <img/>
-                        </td>
+                        <td/>
                     </tr>
+                    {indexedWork[showingStatus].forEach(workItem => {
+                        return (
+                            <tr>
+                                <td>
+                                    {workItem.numOfRegistrants === 0 && <DesignIcon/>}
+                                    {workItem.numOfRegistrants === 1 && <DevelopmentIcon/>}
+                                    {workItem.numOfRegistrants === 2 && <DataScienceIcon/>}
+                                </td>
+                                <td>
+                                    <div className={styles["work-cell-title-inner"]}>
+                                        <div className={styles["work-title"]}>{workItem.name}</div>
+                                        <br/>
+                                        <div className={styles["work-description"]}>
+                                            A dog walking website that allows visitors to select dog walkers and
+                                            schedule dog walking appointments
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    {workItem.rating === 0 &&
+                                        <div className={styles["work-status-circle-draft"]}/>
+                                    }
+                                    {workItem.rating === 1 &&
+                                        <div className={styles["work-status-circle-active"]}/>
+                                    }
+                                    {workItem.rating === 2 &&
+                                        <div className={styles["work-status-circle-submitted"]}/>
+                                    }
+                                    {workItem.rating === 3 &&
+                                        <div className={styles["work-status-circle-in-review"]}/>
+                                    }
+                                    {workItem.rating === 4 &&
+                                        <div className={styles["work-status-circle-redirected"]}/>
+                                    }
+                                    {workItem.rating === 5 &&
+                                        <div className={styles["work-status-circle-cancelled"]}/>
+                                    }
+                                    {workItem.rating === 6 &&
+                                        <div className={styles["work-status-circle-complete"]}/>
+                                    }
+                                    <div className={styles["work-status-text"]}>
+                                        {workStatuses[workItem.rating]}
+                                    </div>
+                                </td>
+                                <td>
+                                    <div className={styles["work-type-text"]}>
+                                        {workTypes[workItem.numOfRegistrants]}
+                                    </div>
+                                </td>
+                                <td>
+                                    <div className={styles["work-created-text"]}>{workItem.created}</div>
+                                </td>
+                                <td>
+                                    <div className={styles["work-solutions-text"]}>April 28, 2022</div>
+                                </td>
+                                <td>
+                                    <div className={styles["work-cost-text"]}>$12,000</div>
+                                </td>
+                                <td>
+                                    {workItem.messagesHasNew &&
+                                        <div className={styles["work-messages-chip-red"]}>
+                                            <div className={styles["work-messages-text"]}>
+                                                {workItem.messagesCount}
+                                            </div>
+                                        </div>
+                                    }
+                                    {!workItem.messagesHasNew &&
+                                        <div className={styles["work-messages-chip-gray"]}>
+                                            <div className={styles["work-messages-text"]}>
+                                                {workItem.messagesCount}
+                                            </div>
+                                        </div>
+                                    }
+                                </td>
+                                <td>
+                                </td>
+                            </tr>
+                        )
+                    })}
                 </tbody>
             </table>
         </ContentLayout>
