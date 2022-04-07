2e5953a162f741329c16d5202b8153a541e6d2a6
brooketopcoder <brooke.souza@topcoder.com>
2022-04-07 13:42:44 -0700

    PROD-1397 #comment remove self service; re-order tools; #time 5m

--- src/tools/tools.routes.ts
+++ src/tools/tools.routes.ts
@@ -1,15 +1,13 @@
 import { PlatformRoute } from '../lib'
 
 import { designLibRoutes } from './design-lib/'
-import { selfServiceRoutes } from './self-service'
 import { workIntakeRoutes } from './work-intake'
 
 const toolRoutes: Array<PlatformRoute> = [
     // NOTE: these will be displayed in the order they are defined in this array
     // TODO: support ordering
-    ...designLibRoutes,
-    ...selfServiceRoutes,
     ...workIntakeRoutes,
+    ...designLibRoutes,
 ]
 
 export default toolRoutes
