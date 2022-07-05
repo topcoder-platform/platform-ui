752f75cb47e24e76635ab20b2b3ef9a4ba3fbc52
Brooke <brooke.souza@topcoder.com>
2022-07-05 15:24:53 -0700

    PROD-2390 #comment add tool to the header if the user is currently on it or has been on it since the SPA was loaded; #time 30m

--- src-ts/header/tool-selectors/tool-selectors-wide/tool-selector-wide/ToolSelectorWide.tsx
+++ src-ts/header/tool-selectors/tool-selectors-wide/tool-selector-wide/ToolSelectorWide.tsx
@@ -19,23 +19,23 @@ const ToolSelectorWide: FC<ToolSelectorWideProps> = (props: ToolSelectorWideProp
 
     const {
         getPathFromRoute,
-        isActiveRoute,
+        isActiveTool,
         isRootRoute,
     }: RouteContextData = useContext(routeContext)
+
     const activePath: string = useLocation().pathname
     const toolRoute: PlatformRoute = props.route
     const toolPath: string = getPathFromRoute(toolRoute)
-
-    const isActive: boolean = isActiveRoute(activePath, toolRoute)
-
-    const activeIndicatorClass: string = `tool-selector-wide-${isActive ? '' : 'in'}active`
+    const baseClass: string = 'tool-selector-wide'
+    const isActive: boolean = isActiveTool(activePath, toolRoute)
+    const activeIndicatorClass: string = `${baseClass}-${isActive ? '' : 'in'}active`
 
     // the tool link should be usable for all active routes except the home page
     const isLink: boolean = isActive && !isRootRoute(activePath)
 
     return (
         <div className={classNames(
-            styles['tool-selector-wide'],
+            styles[baseClass],
             styles[activeIndicatorClass],
             isLink ? styles['tool-selector-wide-is-link'] : undefined
         )}>
