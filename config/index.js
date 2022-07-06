eccf41ab52526269ba270dea7323473f7295231b
Brooke <brooke.souza@topcoder.com>
2022-07-06 12:40:14 -0700

    PROD-2145 #comment add more logging #time 5m

--- config/index.js
+++ config/index.js
@@ -3,7 +3,8 @@
 module.exports = (() => {
   const env = process.env.REACT_APP_HOST_ENV || "dev";
 
-  console.info(`REACT_APP_HOST_ENV: "${env}"`);
+  console.info(`REACT_APP_HOST_ENV: "${process.env.REACT_APP_HOST_ENV}"`);
+  console.info(`env: "${env}"`);
 
   // for security reason don't let to require any arbitrary file defined in process.env
   if (["prod", "dev"].indexOf(env) < 0) {
