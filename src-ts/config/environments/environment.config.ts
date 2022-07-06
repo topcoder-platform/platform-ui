80a43aaf0bfe6830b5797c84c0485971a1eec8ae
Brooke <brooke.souza@topcoder.com>
2022-07-06 10:36:09 -0700

    PROD-2145 more logging #time 5m

--- src-ts/config/environments/environment.config.ts
+++ src-ts/config/environments/environment.config.ts
@@ -8,7 +8,7 @@ import { EnvironmentConfigProd } from './environment.prod.config'
 
 function getEnvironmentConfig(): GlobalConfig {
 
-    console.debug(process.env.REACT_APP_HOST_ENV)
+    console.debug('REACT_APP_HOST_ENV', process.env.REACT_APP_HOST_ENV)
     switch (process.env.REACT_APP_HOST_ENV) {
 
         case AppHostEnvironment.bsouza:
