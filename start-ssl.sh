1d86906532399351c174814328a5a4ddf9f90218
Brooke <brooke.souza@topcoder.com>
2022-07-07 14:33:20 -0700

    PROD-1985 testing pre-commit

--- start-ssl.sh
+++ start-ssl.sh
@@ -3,5 +3,7 @@ export SSL_CRT_FILE=ssl/server.cert
 export SSL_KEY_FILE=ssl/server.key
 export HOST=local.topcoder-dev.com
 export PORT=3003
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 nvm use
 yarn react-app-rewired start
