4be95cb7d45c119e487d03a7d7f4b71ae7479901
Maria Mattlin <maria.mattlin@topcoder.com>
2022-07-15 14:50:56 -0700

    PROD-2434 #comment Added enum for breakpoints and eof lines #time 30m

---
+++ src-ts/lib/styles/variables/breakpoints.enum.ts
@@ -0,0 +1,14 @@
+export enum Breakpoints {
+    xxsMax = 320,
+    xsMin = 321,
+    xsMax = 375,
+    smMin = 376,
+    smMax = 464,
+    mdMin = 465,
+    mdMax = 744,
+    lgMin = 745,
+    lgMax = 984,
+    xlMin = 985,
+    xlMax = 1439,
+    xxlMin = 1440,
+}
