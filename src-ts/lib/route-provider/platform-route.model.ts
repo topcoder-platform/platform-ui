587cec7e4c9bf8a01905d07e73ab5a0c33626241
hentrymartin.p <hentrymartin.p@gmail.com>
2022-07-18 19:03:04 +0200

    PROD-2502 #comment removed hide property from the type #time 10m

--- src-ts/lib/route-provider/platform-route.model.ts
+++ src-ts/lib/route-provider/platform-route.model.ts
@@ -4,7 +4,6 @@ export interface PlatformRoute {
     customerOnly?: boolean
     disabled?: boolean
     element: JSX.Element
-    hide?: boolean
     memberOnly?: boolean
     requireAuth?: boolean
     route: string
