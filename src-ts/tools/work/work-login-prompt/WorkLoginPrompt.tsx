9dcfc1fbb97b99f3d8e888ab238feb02dd134c5b
Dave Armstrong <dave.armstrong@topcoder.com>
2022-07-19 11:07:07 -0500

    PROD-2444 #comment Login Prompt UI #time 30m

---
+++ src-ts/tools/work/work-login-prompt/WorkLoginPrompt.tsx
@@ -0,0 +1,60 @@
+import { FC } from 'react';
+import { NavigateFunction, useNavigate } from 'react-router-dom';
+
+import config from '../../../../config'
+import { Button } from '../../../lib'
+
+import styles from './WorkLoginPrompt.module.scss'
+
+export interface WorkLoginPromptProps {
+    isLoggedIn: boolean,
+    nextPageUrl: string,
+}
+
+const WorkLoginPrompt: FC<WorkLoginPromptProps> = (props: WorkLoginPromptProps) => {
+    const { isLoggedIn, nextPageUrl }: WorkLoginPromptProps = props
+    const navigate: NavigateFunction = useNavigate()
+
+    if (isLoggedIn) {
+        navigate(nextPageUrl)
+        return <></>
+    }
+
+    const onLogin: () => void = () => {
+        window.location.href = config.SIGN_IN_URL
+    }
+
+    const onSignUp: () => void = () => {
+        window.location.href = config.SIGN_UP_URL
+    }
+
+    return (
+        <>
+            <div className={styles.container}>
+                <div className='body-main'>
+                    <h2>
+                        Log in or create an account
+                    </h2>
+                    <p>
+                        You are about to share secured information. To ensure your
+                        security, please log in or create an account.
+                    </p>
+
+                    <div className={styles['btn']}>
+                        <Button
+                            label='LOG IN'
+                            onClick={onLogin}
+                        />
+                        <span className={styles['separator']}>OR</span>
+                        <Button
+                            label='SIGN UP'
+                            onClick={onSignUp}
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default WorkLoginPrompt
