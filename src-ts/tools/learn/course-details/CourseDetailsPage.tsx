e42e7d02466a4ecae3ddc3b6bd9c0e7ca245d743
Vasilica Olariu <olariu.vasilica@gmail.com>
2022-07-20 15:38:23 +0300

    Fix 'cyclomatic-complexity' error lint

--- src-ts/tools/learn/course-details/CourseDetailsPage.tsx
+++ src-ts/tools/learn/course-details/CourseDetailsPage.tsx
@@ -1,4 +1,4 @@
-import { FC, useContext, useMemo } from 'react'
+import { FC, ReactNode, useContext, useMemo } from 'react'
 import { Params, useParams } from 'react-router-dom'
 
 import {
@@ -47,6 +47,52 @@ const CourseDetailsPage: FC<{}> = () => {
         { url: `/learn/${routeParams.provider}/${routeParams.certification}`, name: course?.title ?? '' },
     ], [routeParams, course])
 
+    function getDescription(): ReactNode {
+        if (!course) {
+            return
+        }
+
+        return progress?.status === 'completed' ? (
+            <>
+                <h3 className='details'>Suggested next steps</h3>
+
+                <div className={styles['text']}>
+                    <p>
+                        Now that you have completed the {course.title},
+                        we'd recommend you enroll in another course to continue your learning.
+                        You can view our other courses from the Topcoder Academy course page.
+                    </p>
+                </div>
+            </>
+        ) : (
+            course.keyPoints && (
+                <>
+                    <h3 className='details'>Why should you complete this course?</h3>
+
+                    <div
+                        className={styles['text']}
+                        dangerouslySetInnerHTML={{ __html: (course.keyPoints ?? []).join('<br /><br />') }}
+                    ></div>
+                </>
+            )
+        )
+    }
+
+    function getProviderCredits(): ReactNode {
+        if (!course) {
+            return
+        }
+
+        return course.provider === 'freeCodeCamp' && (
+            <div className={styles['credits-link']}>
+                <a href='https://freecodecamp.org/' target='_blank' referrerPolicy='no-referrer' rel='noreferrer'>
+                    This course was created by the freeCodeCamp.org community.
+                    <IconOutline.ExternalLinkIcon />
+                </a>
+            </div>
+        )
+    }
+
     return (
         <ContentLayout>
             {!ready && (
@@ -59,7 +105,7 @@ const CourseDetailsPage: FC<{}> = () => {
                 <>
                     <div className={styles['wrap']}>
                         <div className={styles['intro-copy']}>
-                            <CourseTitle size='lg' title={course.title} credits={course?.provider} type='webdev' />
+                            <CourseTitle size='lg' title={course.title} credits={course.provider} type='webdev' />
 
                             <div
                                 className={styles['text']}
@@ -68,30 +114,7 @@ const CourseDetailsPage: FC<{}> = () => {
                         </div>
 
                         <div className={styles['description']}>
-                            {progress?.status === 'completed' ? (
-                                <>
-                                    <h3 className='details'>Suggested next steps</h3>
-
-                                    <div className={styles['text']}>
-                                        <p>
-                                            Now that you have completed the {course.title},
-                                            we'd recommend you enroll in another course to continue your learning.
-                                            You can view our other courses from the Topcoder Academy course page.
-                                        </p>
-                                    </div>
-                                </>
-                            ) : (
-                                course.keyPoints && (
-                                    <>
-                                        <h3 className='details'>Why should you complete this course?</h3>
-
-                                        <div
-                                            className={styles['text']}
-                                            dangerouslySetInnerHTML={{ __html: (course.keyPoints ?? []).join('<br /><br />') }}
-                                        ></div>
-                                    </>
-                                )
-                            )}
+                            {getDescription()}
                             <div className={styles['coming-soon']}>
                                 <PromoCourse />
                             </div>
@@ -106,14 +129,7 @@ const CourseDetailsPage: FC<{}> = () => {
                             />
                         </div>
                     </div>
-                    {course?.provider === 'freeCodeCamp' && (
-                        <div className={styles['credits-link']}>
-                            <a href='https://freecodecamp.org/' target='_blank' referrerPolicy='no-referrer' rel='noreferrer'>
-                                This course was created by the freeCodeCamp.org community.
-                                <IconOutline.ExternalLinkIcon />
-                            </a>
-                        </div>
-                    )}
+                    {getProviderCredits()}
                 </>
             )}
         </ContentLayout>
