02d6ebd17927ca250209f7e64a50c5cbf4ad333d
Vasilica Olariu <olariu.vasilica@gmail.com>
2022-07-28 15:53:01 +0300

    lint

--- src-ts/tools/learn/learn-lib/course-badge/badges.functions.ts
+++ src-ts/tools/learn/learn-lib/course-badge/badges.functions.ts
@@ -1,4 +1,5 @@
 import { FC, SVGProps } from 'react'
+
 import { DataScienceBadge, DesignBadge, DevelopBadge, QABadge } from './badges'
 
 const badgesMap: {[key: string]: FC<SVGProps<SVGSVGElement>>} = {
