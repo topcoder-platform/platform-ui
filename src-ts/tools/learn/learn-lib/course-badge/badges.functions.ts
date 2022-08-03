d01f8d6edc1ed6267c7023804de0e238968012ae
Vasilica <olariu.vasilica@gmail.com>
2022-08-03 22:16:59 +0300

    PROD-2754 - fix mobile download certificate

--- src-ts/tools/learn/learn-lib/course-badge/badges.functions.ts
+++ src-ts/tools/learn/learn-lib/course-badge/badges.functions.ts
@@ -1,6 +1,15 @@
 import { FC, SVGProps } from 'react'
 
-import { DataScienceBadge, DesignBadge, DevelopBadge, QABadge } from './badges'
+import {
+    DataScienceBadge,
+    DesignBadge,
+    DevelopBadge,
+    QABadge,
+    DataScienceBadgeImg,
+    DesignBadgeImg,
+    DevelopBadgeImg,
+    QABadgeImg,
+} from './badges'
 
 const badgesMap: {[key: string]: FC<SVGProps<SVGSVGElement>>} = {
     DATASCIENCE: DataScienceBadge,
@@ -9,6 +18,17 @@ const badgesMap: {[key: string]: FC<SVGProps<SVGSVGElement>>} = {
     QA: QABadge,
 }
 
+const badgesImgMap: {[key: string]: string} = {
+    DATASCIENCE: DataScienceBadgeImg,
+    DESIGN: DesignBadgeImg,
+    DEV: DevelopBadgeImg,
+    QA: QABadgeImg,
+}
+
 export function getBadge(badgeType: keyof typeof badgesMap): FC<SVGProps<SVGSVGElement>> {
     return badgesMap[badgeType]
+}
+
+export function getBadgeImg(badgeType: keyof typeof badgesMap): string {
+    return badgesImgMap[badgeType]
 }
