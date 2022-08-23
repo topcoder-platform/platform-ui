170bfb5c8f8084af8f53cd52432297bb20a7c23b
sah2ed <sah2ed@users.noreply.github.com>
2022-08-23 19:20:27 +0100

    Add tiny convenience updates to the README.

--- start-ssl.sh
+++ start-ssl.sh
@@ -2,6 +2,6 @@ export HTTPS=true
 export SSL_CRT_FILE=ssl/server.crt
 export SSL_KEY_FILE=ssl/server.key
 export HOST=local.topcoder-dev.com
-source ~/.nvm/nvm.sh
+source $NVM_DIR/nvm.sh
 nvm use
 yarn react-app-rewired start
