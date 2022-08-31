b8b1953fd747fddce483a121fb88418032cee684
Brooke <brooke.souza@topcoder.com>
2022-08-31 09:39:14 -0700

    TCA-60 #comment This commit purposely creates a failing test in order to review the ci integration #time 15m

--- cypress/e2e/home/home.spec.ts
+++ cypress/e2e/home/home.spec.ts
@@ -1,6 +1,9 @@
 describe('Landing Page', () => {
-  beforeEach(() => cy.visit('/'))
-  it('loads landing page should be successfully', () => {
-    cy.get('[data-cy="root"]').should('be.visible')
-  })
+
+    beforeEach(() => cy.visit('/'))
+
+    it('loads landing page should be successfully', () => {
+        // TCA-60 temp add "not.be.visible" to test ci integration
+        cy.get('[data-cy="root"]').should('not.be.visible')
+    })
 })
