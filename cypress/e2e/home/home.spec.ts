33eac904a50a97c0cde730e3a64eb4f719d84581
LieutenantRoger <gair_2015@hotmail.com>
2022-09-09 15:34:47 +0800

    merge dev

--- cypress/e2e/home/home.spec.ts
+++ cypress/e2e/home/home.spec.ts
@@ -3,11 +3,11 @@ describe('Landing Page', () => {
     beforeEach(() => cy.visit('/'))
 
     // TCA-336 temporarily skip this bc the site isn't loading
-    it.skip('loads landing page should be successfully', () => {
+    it('loads landing page should be successfully', () => {
         cy.get('[data-id="root"]').should('be.visible')
     })
 
-    it.skip('loads landing page should fail', () => {
+    it('loads landing page should fail', () => {
       cy.get('[data-id="root"]').should('not.be.visible')
     })
 })
