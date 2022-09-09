efe4e018107a70e7c665a68bdf15224d23e027d7
Brooke <brooke.souza@topcoder.com>
2022-09-09 08:16:47 -0700

    TCA-408 clean up for dev #time 10m

--- cypress/e2e/home/home.spec.ts
+++ cypress/e2e/home/home.spec.ts
@@ -2,12 +2,11 @@ describe('Landing Page', () => {
 
     beforeEach(() => cy.visit('/'))
 
-    // TCA-336 temporarily skip this bc the site isn't loading
     it('loads landing page should be successfully', () => {
         cy.get('[data-id="root"]').should('be.visible')
     })
 
-    it('loads landing page should fail', () => {
+    it.skip('loads landing page should fail', () => {
       cy.get('[data-id="root"]').should('not.be.visible')
     })
 })
