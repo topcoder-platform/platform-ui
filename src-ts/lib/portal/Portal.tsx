d40524fe251aa6420da91083d71be13c9c98d659
Vasilica Olariu <olariu.vasilica@gmail.com>
2022-10-13 16:31:30 +0300

    fix PORTAL component

--- src-ts/lib/portal/Portal.tsx
+++ src-ts/lib/portal/Portal.tsx
@@ -9,48 +9,37 @@ interface PortalProps {
     portalRef?: MutableRefObject<HTMLElement>,
 }
 
-const Portal: FC<PortalProps> = ({
-    portalId,
-    portalNode,
-    children,
-    className,
-    portalRef,
-}: PortalProps) => {
+const Portal: FC<PortalProps> = (props: PortalProps) => {
+    const portalNode: HTMLElement | null = props.portalId
+        ? document.getElementById(props.portalId)
+        :  props.portalNode ?? null
 
     const defaultPortalNode: HTMLElement = useMemo(() => {
-        if (portalNode) {
+        if (props.portalId || props.portalNode) {
             return
         }
 
-        if (portalId) {
-            return document.getElementById(portalId)
-        }
-
         const backupHtmlNode: HTMLElement = document.createElement('div')
-        if (className) {
-            backupHtmlNode.classList.add(className)
+        if (props.className) {
+            backupHtmlNode.classList.add(props.className)
         }
         document.body.appendChild(backupHtmlNode)
         return backupHtmlNode
-    }, [
-        portalId,
-        portalNode,
-        className,
-    ]) as HTMLElement
+    }, [props.portalId, props.portalNode, props.className]) as HTMLElement
 
     useEffect(() => {
         return () => {
-            if (defaultPortalNode) {
+            if (defaultPortalNode && document.body.contains(defaultPortalNode)) {
                 document.body.removeChild(defaultPortalNode)
             }
         }
     }, [defaultPortalNode])
 
-    if (portalRef) {
-        portalRef.current = portalNode ?? defaultPortalNode
+    if (props.portalRef) {
+        props.portalRef.current = portalNode ?? defaultPortalNode
     }
 
-    return createPortal(children, portalNode ?? defaultPortalNode)
+    return createPortal(props.children, portalNode ?? defaultPortalNode)
 }
 
 export default Portal
