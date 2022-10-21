6ad582d63e5121b1d3193eb9055981470fc1b0eb
Brooke <brooke.souza@topcoder.com>
2022-10-21 14:35:50 -0700

    TCA-584 #comment This commit adds support for the UI to tell the API to generate alternate versions of images. #time 1h

--- src-ts/tools/learn/learn-config/learn.default.config.ts
+++ src-ts/tools/learn/learn-config/learn.default.config.ts
@@ -2,6 +2,9 @@ import { LearnConfigModel } from './learn-config.model'
 
 export const LearnConfigDefault: LearnConfigModel = {
     API: 'http://localhost:3001/v5/learning-paths',
+    CERT_ALT_PARAMS: {
+        'view-port': 'large-container',
+    },
     CERT_DOMAIN: 'https://certificate.topcoder-dev.com',
     CERT_ELEMENT_SELECTOR: {
         attribute: 'data-id',
