21f4512418257361d99027e4b3363fa49a58fd46
Vasilica Olariu <olariu.vasilica@gmail.com>
2022-11-14 11:16:02 +0200

    PR feedback

--- src-ts/tools/learn/welcome/progress-block/progress-action/ProgressAction.tsx
+++ src-ts/tools/learn/welcome/progress-block/progress-action/ProgressAction.tsx
@@ -43,7 +43,7 @@ const ProgressAction: FC<ProgressActionProps> = (props: ProgressActionProps) => 
     const certificationsById: { [key: string]: LearnCertification } = useMemo(() => (
         props.allCertifications
             .reduce((certifs, certificate) => {
-                set(certifs, [certificate.id], certificate)
+                certifs[certificate.id] = certificate
                 return certifs
             }, {} as unknown as { [key: string]: LearnCertification })
     ), [props.allCertifications])
