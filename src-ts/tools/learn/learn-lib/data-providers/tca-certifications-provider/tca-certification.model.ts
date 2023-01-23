beb21186f961f6d45594bdda0c99f202930c9a4a
Kiril Kartunov <kiril.kartunov@gmail.com>
2023-01-23 12:11:15 +0200

    TCA-870 update TCA certs model to latest per API

--- src-ts/tools/learn/learn-lib/data-providers/tca-certifications-provider/tca-certification.model.ts
+++ src-ts/tools/learn/learn-lib/data-providers/tca-certifications-provider/tca-certification.model.ts
@@ -1,15 +1,23 @@
 import { TCACertificationLearnLevel } from './tca-certificate-level-type'
 import { TCACertificationStatus } from './tca-certificate-status-type'
+import { TCACertificationCategory } from './tca-certification-category.model'
 
 export interface TCACertification {
     id: number
     title: string
+    dashedName: string
     description: string
+    introText: string
     estimatedCompletionTime: number
     status: TCACertificationStatus
     sequentialCourses: boolean
     learnerLevel: TCACertificationLearnLevel
     certificationCategoryId: string
+    certificationCategory: TCACertificationCategory
     stripeProductId?: string
     skills: string[]
+    learningOutcomes: string[]
+    prerequisites: string[]
+    createdAt: Date
+    updatedAt: Date
 }
