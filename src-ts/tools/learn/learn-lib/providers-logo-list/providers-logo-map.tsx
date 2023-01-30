4dd0be79d6ade04a4313f2dc4399c93977378b5a
Kiril Kartunov <kiril.kartunov@gmail.com>
2023-01-30 13:56:03 +0200

    remove commented code

--- src-ts/tools/learn/learn-lib/providers-logo-list/providers-logo-map.tsx
+++ src-ts/tools/learn/learn-lib/providers-logo-list/providers-logo-map.tsx
@@ -1,8 +1,7 @@
 import { ReactNode } from 'react'
+
 import { FccLogoBlackSvg, TcLogoSvg } from '../svgs'
 import { TCAProviderType } from '../data-providers'
-
-// import { FccLogoBlackSvg, TCAProviderType, TcLogoSvg } from '../index'
 
 export const providersLogoMap: {[key in TCAProviderType]: ReactNode} = {
     freeCodeCamp: <FccLogoBlackSvg />,
