609a1406e551312a4a8bb8e8b7dbb859973a9da0
Vasilica Olariu <olariu.vasilica@gmail.com>
2023-03-13 10:11:43 +0200

    TCA-1141 - checks if tcCertification is valid

--- src-ts/tools/learn/learn-lib/data-providers/tca-certifications-provider/tca-certification/tca-certification-data.model.ts
+++ src-ts/tools/learn/learn-lib/data-providers/tca-certifications-provider/tca-certification/tca-certification-data.model.ts
@@ -1,7 +1,7 @@
 import { TCACertification } from '../tca-certification.model'
 
 export interface TCACertificationProviderData {
-    certification: TCACertification
+    certification?: TCACertification
     error: boolean
     loading: boolean
     ready: boolean
