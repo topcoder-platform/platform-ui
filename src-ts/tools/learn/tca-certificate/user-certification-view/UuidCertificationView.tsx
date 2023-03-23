957b150fbc7160a45d6e785b0fe2f29cf4ab9bb6
Vasilica Olariu <olariu.vasilica@gmail.com>
2023-03-23 14:18:18 +0200

    TCA-1189 - HM view based on uuid: redirect to full url

--- src-ts/tools/learn/tca-certificate/user-certification-view/UuidCertificationView.tsx
+++ src-ts/tools/learn/tca-certificate/user-certification-view/UuidCertificationView.tsx
@@ -1,36 +1,52 @@
-import { FC } from 'react'
-import { Params, useParams } from 'react-router-dom'
+import { FC, useLayoutEffect } from 'react'
+import { NavigateFunction, Params, useNavigate, useParams } from 'react-router-dom'
 
+import { LoadingSpinner } from '../../../../lib'
 import {
     TCACertification,
     TCACertificationEnrollmentProviderData,
     useTCACertificationEnrollment,
 } from '../../learn-lib'
+import { getTCAUserCertificationUrl } from '../../learn.routes'
 
-import { useGetUserProfile, UseGetUserProfileData } from './use-get-user-profile'
 import UserCertificationViewBase from './UserCertificationViewBase'
 
 const UuidCertificationView: FC<{}> = () => {
-
+    const navigate: NavigateFunction = useNavigate()
     const routeParams: Params<string> = useParams()
 
     const {
         enrollment,
         error: enrollmentError,
+        ready: enrollmentReady,
     }: TCACertificationEnrollmentProviderData
         = useTCACertificationEnrollment(routeParams.completionUuid as string)
 
-    const { profile }: UseGetUserProfileData = useGetUserProfile(enrollment?.userHandle)
-
     const certification: TCACertification | undefined = enrollment?.topcoderCertification
 
+    useLayoutEffect(() => {
+        if (enrollmentReady && enrollment) {
+            navigate(
+                getTCAUserCertificationUrl(
+                    certification?.dashedName as string,
+                    enrollment.userHandle,
+                ),
+            )
+        }
+    }, [certification?.dashedName, enrollment, enrollmentReady, navigate])
+
     return (
-        <UserCertificationViewBase
-            enrollment={enrollment}
-            certification={certification}
-            profile={profile}
-            enrollmentError={enrollmentError}
-        />
+        <>
+            <LoadingSpinner hide={enrollmentReady} />
+            {enrollmentReady && enrollmentError && (
+                <UserCertificationViewBase
+                    enrollment={enrollment}
+                    certification={certification}
+                    profile={{} as any}
+                    enrollmentError={enrollmentError}
+                />
+            )}
+        </>
     )
 }
 
