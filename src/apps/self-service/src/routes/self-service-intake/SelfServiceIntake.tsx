70660cb3dcf68eb8007758f184d196a05f9189d1
Vasilica Olariu <olariu.vasilica@gmail.com>
2023-04-24 10:23:47 +0300

    Add self-service -> reload work

--- src/apps/self-service/src/routes/self-service-intake/SelfServiceIntake.tsx
+++ src/apps/self-service/src/routes/self-service-intake/SelfServiceIntake.tsx
@@ -2,20 +2,30 @@ import { FC, useContext } from 'react'
 import { Provider } from 'react-redux'
 import { Outlet, Routes } from 'react-router-dom'
 
-import { ContentLayout } from '~/libs/ui'
+import { ContentLayout, LoadingSpinner } from '~/libs/ui'
 import { routerContext, RouterContextData } from '~/libs/core'
 
 import { WorkProvider } from '../../lib'
 import { intakeFormsRouteId } from '../../self-service.routes'
 import store from '../../store'
+import { useLoadUnfinishedWork } from './use-load-unfinished-work'
 
 const SelfServiceIntake: FC<{}> = () => {
+    const { isLoading } = useLoadUnfinishedWork();
+
+    return (
+        <LoadingSpinner hide={!isLoading} />
+    )
+}
+
+const SelfServiceIntakeWrapper: FC<{}> = () => {
     const { getChildRoutes }: RouterContextData = useContext(routerContext)
 
     return (
         <ContentLayout>
             <Provider store={store}>
                 <WorkProvider>
+                    <SelfServiceIntake />
                     <Outlet />
                     <Routes>
                         {getChildRoutes(intakeFormsRouteId)}
@@ -26,4 +36,4 @@ const SelfServiceIntake: FC<{}> = () => {
     )
 }
 
-export default SelfServiceIntake
+export default SelfServiceIntakeWrapper
