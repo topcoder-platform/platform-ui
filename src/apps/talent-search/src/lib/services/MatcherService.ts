80f33ab4e85e01413e19700ed1cc5de54fbc0b35
Justin Gasper <jmgasper@gmail.com>
2023-06-02 14:26:38 +1000

    Refactor due to weird issues on deployed server.

--- src/apps/talent-search/src/lib/services/MatcherService.ts
+++ src/apps/talent-search/src/lib/services/MatcherService.ts
@@ -1,26 +1,36 @@
+/* eslint-disable ordered-imports/ordered-imports */
+/* eslint-disable space-before-blocks */
+/* eslint-disable @typescript-eslint/typedef */
 import { EnvironmentConfig } from '~/config'
 import { xhrGetAsync } from '~/libs/core'
-import { Member, Skill } from '@talentSearch/lib/models'
 
-export default class MatcherService {
-    public static async autoCompleteSkills(search:string): Promise<Array<Skill>> {
-        return xhrGetAsync(`${EnvironmentConfig.API.V5}/emsi-skills/skills/auto-complete?term=${search}`)
-    }
+import Skill from '@talentSearch/lib/models/Skill'
+import Member from '@talentSearch/lib/models/Member'
 
-    public static async retrieveMatchesForSkills(
-        skills:ReadonlyArray<Skill>,
-        page:number,
-        pageSize:number,
-    ): Promise<Array<Member>> {
-        const params: URLSearchParams = new URLSearchParams()
-        skills.forEach(value => params.append('skillId', value.emsiId))
-        params.append('sortBy', 'numberOfChallengesWon')
-        params.append('sortOrder', 'desc')
-        params.append('page', `${page}`)
-        params.append('perPage', `${pageSize}`)
-
-        const url: string = `${EnvironmentConfig.API.V5}/members/searchBySkills?${params.toString()}`
-
-        return xhrGetAsync(url)
-    }
+export async function autoCompleteSkills(search:string): Promise<Array<Skill>>{
+    return xhrGetAsync(`${EnvironmentConfig.API.V5}/emsi-skills/skills/auto-complete?term=${search}`)
 }
+
+export async function retrieveMatchesForSkills(
+    skills:ReadonlyArray<Skill>,
+    page:number,
+    pageSize:number,
+): Promise<Array<Member>>{
+    const params = new URLSearchParams()
+    skills.forEach(value => params.append('skillId', value.emsiId))
+    params.append('sortBy', 'numberOfChallengesWon')
+    params.append('sortOrder', 'desc')
+    params.append('page', `${page}`)
+    params.append('perPage', `${pageSize}`)
+
+    const url = `${EnvironmentConfig.API.V5}/members/searchBySkills?${params.toString()}`
+
+    return xhrGetAsync(url)
+}
+
+const MatcherService = {
+    autoCompleteSkills,
+    retrieveMatchesForSkills,
+}
+
+export default MatcherService
