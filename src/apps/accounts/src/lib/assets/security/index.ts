42d49af105f5857be192f7fbcf89bb9fe82f044d
Kiril Kartunov <kiril.kartunov@gmail.com>
2023-06-15 13:19:00 +0300

    MP-40 dice modal wrap up

--- src/apps/accounts/src/lib/assets/security/index.ts
+++ src/apps/accounts/src/lib/assets/security/index.ts
@@ -1,13 +1,17 @@
 import { ReactComponent as MFAImage } from './mfa.svg'
 import { ReactComponent as AppleStore } from './apple-store.svg'
+import { ReactComponent as UnSuccessfullDiceVerificationIcon } from './unsuccessful.svg'
 import diceIdLogo from './dicelogo.png'
 import diceIdLogoBig from './dicelogobig.png'
+import diceIdLogoSmall from './dicelogosmall.png'
 import googlePlay from './google-play.png'
 
 export {
     AppleStore,
     diceIdLogo,
     diceIdLogoBig,
+    diceIdLogoSmall,
     googlePlay,
     MFAImage,
+    UnSuccessfullDiceVerificationIcon,
 }
