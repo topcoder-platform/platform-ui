2ff8c3eb04db9a32c1e90da486641b3dcc57e039
Kiril Kartunov <kiril.kartunov@gmail.com>
2023-07-10 10:31:08 +0300

    MP-128 member location and local time

--- src/apps/profiles/src/member-profile/local-info/MemberLocalInfo.tsx
+++ src/apps/profiles/src/member-profile/local-info/MemberLocalInfo.tsx
@@ -1,4 +1,5 @@
-import { FC } from 'react'
+import { FC, useMemo } from 'react'
+import cityTimezones from 'city-timezones'
 import moment from 'moment-timezone'
 
 import { useCountryName, UserProfile } from '~/libs/core'
@@ -12,22 +13,46 @@ interface MemberLocalInfoProps {
 
 const MemberLocalInfo: FC<MemberLocalInfoProps> = (props: MemberLocalInfoProps) => {
 
-    const memberCountry: string | undefined = useCountryName(props.profile?.homeCountryCode)
+    const memberCountry: string | undefined
+        = useCountryName(props.profile?.homeCountryCode || props.profile?.competitionCountryCode)
+
+    const city: string | undefined = props.profile?.addresses?.[0]?.city
+
+    const memberCityTimezone: string | undefined = useMemo(() => {
+        if (!city) {
+            return undefined
+        }
+
+        const cityTimezoneData: cityTimezones.CityData[] = cityTimezones.lookupViaCity(city)
+        let memberTimezone: string | undefined
+
+        if (!cityTimezoneData?.length) {
+            memberTimezone = `${memberCountry}/${city}`
+        } else {
+            memberTimezone = cityTimezoneData[0].timezone
+        }
+
+        return moment.tz.zone(memberTimezone) ? memberTimezone : undefined
+    }, [city, memberCountry])
 
     return (
         <div className={styles.container}>
             <div className={styles.localInfo}>
                 <IconSolid.LocationMarkerIcon />
-                {memberCountry}
+                {`${!!city ? `${city}, ` : ''}${memberCountry}`}
             </div>
-            <div className={styles.localInfo}>
-                <IconSolid.ClockIcon />
-                Local time:
-                {' '}
-                {moment()
-                    .tz('America/New_York')
-                    .format('HH:MM')}
-            </div>
+            {
+                !!memberCityTimezone && (
+                    <div className={styles.localInfo}>
+                        <IconSolid.ClockIcon />
+                        Local time:
+                        {' '}
+                        {moment()
+                            .tz(memberCityTimezone)
+                            .format('hh:mm')}
+                    </div>
+                )
+            }
         </div>
     )
 }
