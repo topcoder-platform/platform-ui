fd080e1f1185fa93e65708cb38eeec30de3d744b
Vasilica <olariu.vasilica@gmail.com>
2023-07-12 00:33:35 +0300

    react-env: convert env variables to bool/number/null

--- src/config/environments/react-env.ts
+++ src/config/environments/react-env.ts
@@ -1,11 +1,27 @@
+/* eslint-disable unicorn/no-null */
 /* eslint-disable @typescript-eslint/typedef */
 
 export function getReactEnv<T>(varName: string, defaultValue?: string | boolean | number): T {
     const hasDefaultValue: boolean = arguments.length > 1
-    const value = process.env[`REACT_APP_${varName}`]
+    let value = process.env[`REACT_APP_${varName}`] as unknown as T
 
     if (value === undefined && !hasDefaultValue) {
         throw new Error(`${varName} is not defined in process.env!`)
+    }
+
+    // convert to boolean
+    if (value === 'false' || value === 'true') {
+        value = (value === 'true' as unknown) as T
+    }
+
+    // convert to null
+    if (value === 'null') {
+        value = (null as unknown) as T
+    }
+
+    // convert to number
+    if (!Number.isNaN(Number(value))) {
+        value = (null as unknown) as T
     }
 
     return (value ?? defaultValue) as T
