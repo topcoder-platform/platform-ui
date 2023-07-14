a026558a116bf7f6a83da1f9059ac06d019b8b0b
Kiril Kartunov <kiril.kartunov@gmail.com>
2023-07-14 06:39:10 +0300

    MP-90 breakdown challenge wins per track - init

--- src/apps/profiles/src/member-profile/tc-achievements/ChallengeWinsBanner/ChallengeWinsBanner.tsx
+++ src/apps/profiles/src/member-profile/tc-achievements/ChallengeWinsBanner/ChallengeWinsBanner.tsx
@@ -1,20 +1,71 @@
 import { FC } from 'react'
 
+import { MemberStats, UserStats } from '~/libs/core'
+
+import { subTrackLabelToHumanName } from '../../../lib/helpers'
+
 import styles from './ChallengeWinsBanner.module.scss'
 
 interface ChallengeWinsBannerProps {
-    challengeWins: number
+    memberStats: UserStats
 }
 
 const ChallengeWinsBanner: FC<ChallengeWinsBannerProps> = (props: ChallengeWinsBannerProps) => (
     <div className={styles.containerWrap}>
         <div className={styles.container}>
             <p className='body-large-bold'>Topcoder Challenge Winner</p>
-            <p className={styles.wins}>
-                {props.challengeWins}
-                {' '}
-            </p>
-            <p className='body-main'>WINS</p>
+            <div className={styles.wins}>
+                {
+                    !!props.memberStats.DATA_SCIENCE?.SRM?.wins && (
+                        <div className={styles.winWrapper}>
+                            <p className={styles.winCnt}>
+                                {props.memberStats.DATA_SCIENCE.SRM.wins}
+                                {' '}
+                            </p>
+                            <p className='body-main'>WINS</p>
+                            <p className='body-main-bold'>SRM</p>
+                        </div>
+                    )
+                }
+                {
+                    !!props.memberStats.DATA_SCIENCE?.MARATHON_MATCH?.wins && (
+                        <div className={styles.winWrapper}>
+                            <p className={styles.winCnt}>
+                                {props.memberStats.DATA_SCIENCE.MARATHON_MATCH.wins}
+                                {' '}
+                            </p>
+                            <p className='body-main'>WINS</p>
+                            <p className='body-main-bold'>Marathon Match</p>
+                        </div>
+                    )
+                }
+                {
+                    !!props.memberStats.DEVELOP?.wins
+                    && props.memberStats.DEVELOP?.subTracks.map((ms: MemberStats) => (ms.wins ? (
+                        <div className={styles.winWrapper}>
+                            <p className={styles.winCnt}>
+                                {ms.wins}
+                                {' '}
+                            </p>
+                            <p className='body-main'>WINS</p>
+                            <p className='body-main-bold'>{subTrackLabelToHumanName(ms.name)}</p>
+                        </div>
+                    ) : undefined))
+                }
+                {
+                    !!props.memberStats.DESIGN?.wins
+                    && props.memberStats.DESIGN?.subTracks.map((ms: MemberStats) => (ms.wins ? (
+                        <div className={styles.winWrapper}>
+                            <p className={styles.winCnt}>
+                                {ms.wins}
+                                {' '}
+                            </p>
+                            <p className='body-main'>WINS</p>
+                            <p className='body-main-bold'>{subTrackLabelToHumanName(ms.name)}</p>
+                        </div>
+                    ) : undefined))
+                }
+            </div>
         </div>
         <p>
             Topcoder challenges are open competitions where community
