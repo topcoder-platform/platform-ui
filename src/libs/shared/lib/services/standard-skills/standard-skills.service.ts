1410318687e003fecc5836e3a1ece0811d895e30
Vasilica Olariu <olariu.vasilica@gmail.com>
2023-10-17 11:21:11 +0300

    TSJR-14 - update api endpoints for skills

--- src/libs/shared/lib/services/standard-skills/standard-skills.service.ts
+++ src/libs/shared/lib/services/standard-skills/standard-skills.service.ts
@@ -1,7 +1,7 @@
 import { EnvironmentConfig } from '~/config'
 import { UserSkill, xhrGetAsync, xhrPostAsync, xhrPutAsync } from '~/libs/core'
 
-const baseUrl = `${EnvironmentConfig.API.V5}/emsi-skills/member-emsi-skills`
+const baseUrl = `${EnvironmentConfig.API.V5}/standardized-skills`
 
 export async function autoCompleteSkills(queryTerm: string): Promise<UserSkill[]> {
     if (!queryTerm) {
@@ -9,7 +9,7 @@ export async function autoCompleteSkills(queryTerm: string): Promise<UserSkill[]
     }
 
     const encodedQuery = encodeURIComponent(queryTerm)
-    return xhrGetAsync(`${EnvironmentConfig.API.V5}/emsi-skills/skills/auto-complete?term=${encodedQuery}`)
+    return xhrGetAsync(`${baseUrl}/skills/autocomplete?term=${encodedQuery}`)
 }
 
 export type FetchMemberSkillsConfig = {
@@ -19,19 +19,18 @@ export async function fetchMemberSkills(
     userId: string | number | undefined,
     config: FetchMemberSkillsConfig,
 ): Promise<UserSkill[]> {
-    const url = `${baseUrl}/${userId}?pageFlag=${!config.skipPagination}`
+    const url = `${baseUrl}/user-skills/${userId}?disablePagination=${config.skipPagination}`
     return xhrGetAsync(url)
 }
 
 export async function createMemberSkills(userId: number, skills: UserSkill[]): Promise<void> {
-    return xhrPostAsync(baseUrl, {
+    return xhrPostAsync(`${baseUrl}/user-skills/${userId}`, {
         skills,
-        userId,
     })
 }
 
 export async function updateMemberSkills(userId: string | number, skills: UserSkill[]): Promise<void> {
-    return xhrPutAsync(`${baseUrl}/${userId}`, {
+    return xhrPutAsync(`${baseUrl}/user-skills/${userId}`, {
         skills,
     })
 }
