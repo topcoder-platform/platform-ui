503a433db1db8f72f09aa1f774c50b615f0e76db
Vasilica Olariu <olariu.vasilica@gmail.com>
2023-12-06 21:58:09 +0200

    update comment

--- src/apps/profiles/src/lib/helpers.ts
+++ src/apps/profiles/src/lib/helpers.ts
@@ -118,8 +118,8 @@ export function isValidURL(urlToValidate: string): boolean {
  * Creates the string with the number of items and the word describing the item
  * possibly in plural form.
  *
- * @param {number} count number of entities
- * @param {string} baseWord base word that describes the entity
+ * @param {number} count - The number of entities
+ * @param {string} baseWord - The base word that describes the entity
  * @returns {string}
  */
 export function formatPlural(count: number, baseWord: string): string {
