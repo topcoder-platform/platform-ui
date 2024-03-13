220f7dd99a62ed3673e245ae72d324deebc73146
Rakib Ansary <rakibansary@topcoder.com>
2024-03-13 17:18:58 +0600

    fix: lint

--- src/apps/wallet-admin/src/home/tabs/home/HomeTab.tsx
+++ src/apps/wallet-admin/src/home/tabs/home/HomeTab.tsx
@@ -1,14 +1,9 @@
 /* eslint-disable react/jsx-wrap-multilines */
-import { FC, useEffect, useState } from 'react'
+import { FC } from 'react'
 
 import { UserProfile } from '~/libs/core'
-import { IconOutline, LinkButton, LoadingCircles } from '~/libs/ui'
 
-import { Balance, WalletDetails } from '../../../lib/models/WalletDetails'
-import { getWalletDetails } from '../../../lib/services/wallet'
-import { InfoRow } from '../../../lib'
 import { BannerImage, BannerText } from '../../../lib/assets/home'
-import Chip from '../../../lib/components/chip/Chip'
 
 import styles from './Home.module.scss'
 
