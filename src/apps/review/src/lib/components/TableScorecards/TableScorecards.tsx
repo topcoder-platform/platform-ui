18e9c0098295f25163f0eee169bc4b984f184e8e
himaniraghav3 <himaniraghav0311@gmail.com>
2025-08-19 14:06:57 +0530

    Fix pagination text and no records message

--- src/apps/review/src/lib/components/TableScorecards/TableScorecards.tsx
+++ src/apps/review/src/lib/components/TableScorecards/TableScorecards.tsx
@@ -33,6 +33,9 @@ export const TableScorecards: FC<Props> = (props: Props) => {
     const { width: screenWidth }: WindowSize = useWindowSize()
     const isTablet = useMemo(() => screenWidth <= 1000, [screenWidth])
 
+    const start = (props.page - 1) * (props.perPage ?? 0) + 1
+    const end = Math.min(props.page * (props.perPage ?? 0), props.metadata?.total ?? 0)
+
     const columns = useMemo<TableColumn<Scorecard>[]>(
         () => [
             {
@@ -153,12 +156,15 @@ export const TableScorecards: FC<Props> = (props: Props) => {
             )}
             <div className={styles.pagination}>
                 <div className={styles.paginationText}>
-                    Showing 1-
-                    {props.perPage}
+                    Showing
+                    {' '}
+                    {start}
+                    -
+                    {end}
                     {' '}
                     of
                     {' '}
-                    {props.metadata.total}
+                    {props.metadata?.total ?? 0}
                     {' '}
                     results
                 </div>
