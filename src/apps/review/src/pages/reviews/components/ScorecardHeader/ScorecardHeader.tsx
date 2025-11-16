cc781f38afa33318b7c6db0ca008b3d194251852
Vasilica Olariu <olariu.vasilica@gmail.com>
2025-11-16 22:05:56 +0200

    noopener

--- src/apps/review/src/pages/reviews/components/ScorecardHeader/ScorecardHeader.tsx
+++ src/apps/review/src/pages/reviews/components/ScorecardHeader/ScorecardHeader.tsx
@@ -73,7 +73,7 @@ const ScorecardHeader: FC = () => {
                                 <strong>Git log:</strong>
                                 {' '}
                                 {workflowRun.gitRunUrl && (
-                                    <a href={workflowRun.gitRunUrl} target='_blank' rel='noreferrer'>
+                                    <a href={workflowRun.gitRunUrl} target='_blank' rel='noreferrer noopener'>
                                         #
                                         {workflowRun.gitRunId}
                                     </a>
