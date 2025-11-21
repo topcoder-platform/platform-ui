1cb6a4572979bf0db375c6e342e14bc6e62a0c68
Hentry Martin <hentrymartin.p@gmail.com>
2025-11-21 03:27:01 +0100

    feat: comments implementation

--- src/apps/review/src/lib/components/Scorecard/ScorecardViewer/ScorecardQuestion/AiFeedback/AiFeedback.tsx
+++ src/apps/review/src/lib/components/Scorecard/ScorecardViewer/ScorecardQuestion/AiFeedback/AiFeedback.tsx
@@ -1,7 +1,7 @@
-import { FC, useMemo } from 'react'
+import { FC, useCallback, useMemo, useState } from 'react'
 
 import { IconAiReview } from '~/apps/review/src/lib/assets/icons'
-import { ScorecardQuestion } from '~/apps/review/src/lib/models'
+import { ReviewsContextModel, ScorecardQuestion } from '~/apps/review/src/lib/models'
 
 import { ScorecardViewerContextValue, useScorecardViewerContext } from '../../ScorecardViewer.context'
 import { ScorecardQuestionRow } from '../ScorecardQuestionRow'
@@ -11,6 +11,11 @@ import { AiFeedbackActions } from '../AiFeedbackActions/AiFeedbackActions'
 import { AiFeedbackComments } from '../AiFeedbackComments/AiFeedbackComments'
 
 import styles from './AiFeedback.module.scss'
+import { AiFeedbackReply } from '../AiFeedbackReply/AiFeedbackReply'
+import { createFeedbackComment } from '~/apps/review/src/lib/services'
+import { useReviewsContext } from '~/apps/review/src/pages/reviews/ReviewsContext'
+import { mutate } from 'swr'
+import { EnvironmentConfig } from '~/config'
 
 interface AiFeedbackProps {
     question: ScorecardQuestion
@@ -21,8 +26,22 @@ const AiFeedback: FC<AiFeedbackProps> = props => {
     const feedback: any = useMemo(() => (
         aiFeedbackItems?.find((r: any) => r.scorecardQuestionId === props.question.id)
     ), [props.question.id, aiFeedbackItems])
+    const { workflowId, workflowRun }: ReviewsContextModel = useReviewsContext()
+    const [showReply, setShowReply] = useState(false)
 
     const commentsArr: any[] = (feedback?.comments) || []
+
+    const onShowReply = useCallback(() => {
+        setShowReply(!showReply)
+    }, [])
+
+    const onSubmitReply = useCallback(async (content: string) => {
+        await createFeedbackComment(workflowId as string, workflowRun?.id as string, feedback?.id, {
+            content,
+        })
+        await mutate(`${EnvironmentConfig.API.V6}/workflows/${workflowId}/runs/${workflowRun?.id}/items`)
+        setShowReply(false)
+    }, [workflowId, workflowRun?.id, feedback?.id])
 
     if (!aiFeedbackItems?.length || !feedback) {
         return <></>
@@ -50,10 +69,18 @@ const AiFeedback: FC<AiFeedbackProps> = props => {
 
             <MarkdownReview value={feedback.content} />
 
-            <AiFeedbackActions feedback={feedback} actionType='runItem' />
+            <AiFeedbackActions feedback={feedback} actionType='runItem' onPressReply={onShowReply} />
+
+            {
+                showReply && (
+                    <AiFeedbackReply onSubmitReply={onSubmitReply} onCloseReply={function closeReply() {
+                        setShowReply(false)
+                    }} />
+                )
+            }
 
             {commentsArr.length > 0 && (
-                <AiFeedbackComments comments={commentsArr} feedback={feedback} />
+                <AiFeedbackComments comments={commentsArr} feedback={feedback} isRoot />
             )}
         </ScorecardQuestionRow>
     )
