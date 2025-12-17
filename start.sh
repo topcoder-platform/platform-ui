4fcb67f899036857e932b5a627e160016e6508e3
Justin Gasper <jmgasper@gmail.com>
2025-12-17 15:28:12 +1100

    Revert start.sh dev changes

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@
 export ESLINT_NO_DEV_ERRORS=true
 export HTTPS=true
 export PORT=443
+export REACT_APP_HOST_ENV=${REACT_APP_HOST_ENV:-dev}
 export HOST=local.topcoder-dev.com
 # if [[ ! -e ./.environments/.env.local ]]; then
 #     filename=.env.${REACT_APP_HOST_ENV:-dev}
