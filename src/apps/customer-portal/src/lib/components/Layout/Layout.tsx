cafa6184677a896dc1bc30e3e754acb176557691
himaniraghav3 <himaniraghav0311@gmail.com>
2026-01-21 18:11:50 +0530

    Optimum fix

--- src/apps/customer-portal/src/lib/components/Layout/Layout.tsx
+++ src/apps/customer-portal/src/lib/components/Layout/Layout.tsx
@@ -1,12 +1,8 @@
-import { FC, PropsWithChildren, useContext, useMemo } from 'react'
+import { FC, PropsWithChildren } from 'react'
 
 import { ContentLayout } from '~/libs/ui'
-import { RestrictedPage } from '~/libs/shared'
 
 import { NavTabs } from '../NavTabs'
-import { CustomerPortalAppContext } from '../../contexts'
-import { CustomerPortalAppContextModel } from '../../models'
-import { PRIVILEGED_ROLES } from '../../../config/index.config'
 
 import styles from './Layout.module.scss'
 
@@ -14,37 +10,18 @@ export const NullLayout: FC<PropsWithChildren> = props => (
     <>{props.children}</>
 )
 
-export const Layout: FC<PropsWithChildren> = props => {
-    const { loginUserInfo }: CustomerPortalAppContextModel = useContext(CustomerPortalAppContext)
-    const userRoles = useMemo(() => loginUserInfo?.roles || [], [loginUserInfo?.roles])
-    const isAuthResolved = loginUserInfo !== undefined
-
-    const isUnprivilegedUser = useMemo(() => {
-        if (!isAuthResolved) return false
-        if (!loginUserInfo) return true
-
-        return !userRoles.some(role => PRIVILEGED_ROLES.includes(role))
-    }, [isAuthResolved, loginUserInfo, userRoles])
-
-    if (isUnprivilegedUser) {
-        return (
-            <RestrictedPage />
-        )
-    }
-
-    return (
-        <>
-            <NavTabs />
-            <ContentLayout
-                innerClass={styles.contantentLayoutInner}
-                outerClass={styles.contentLayoutOuter}
-            >
-                <div className={styles.layout}>
-                    <div className={styles.main}>{props.children}</div>
-                </div>
-            </ContentLayout>
-        </>
-    )
-}
+export const Layout: FC<PropsWithChildren> = props => (
+    <>
+        <NavTabs />
+        <ContentLayout
+            innerClass={styles.contentLayoutInner}
+            outerClass={styles.contentLayoutOuter}
+        >
+            <div className={styles.layout}>
+                <div className={styles.main}>{props.children}</div>
+            </div>
+        </ContentLayout>
+    </>
+)
 
 export default Layout
