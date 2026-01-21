cd4f0fba577fabf94f3f10a676ba8eefdb453f49
himaniraghav3 <himaniraghav0311@gmail.com>
2026-01-21 16:54:03 +0530

    PM-3452 Show restricted page for unprivileged users

--- src/apps/customer-portal/src/lib/components/Layout/Layout.tsx
+++ src/apps/customer-portal/src/lib/components/Layout/Layout.tsx
@@ -1,8 +1,12 @@
-import { FC, PropsWithChildren } from 'react'
+import { FC, PropsWithChildren, useContext, useMemo } from 'react'
 
 import { ContentLayout } from '~/libs/ui'
+import { RestrictedPage } from '~/libs/shared'
 
 import { NavTabs } from '../NavTabs'
+import { CustomerPortalAppContext } from '../../contexts'
+import { CustomerPortalAppContextModel } from '../../models'
+import { PRIVILEGED_ROLES } from '../../../config/index.config'
 
 import styles from './Layout.module.scss'
 
@@ -10,18 +14,34 @@ export const NullLayout: FC<PropsWithChildren> = props => (
     <>{props.children}</>
 )
 
-export const Layout: FC<PropsWithChildren> = props => (
-    <>
-        <NavTabs />
-        <ContentLayout
-            innerClass={styles.contantentLayoutInner}
-            outerClass={styles.contentLayoutOuter}
-        >
-            <div className={styles.layout}>
-                <div className={styles.main}>{props.children}</div>
-            </div>
-        </ContentLayout>
-    </>
-)
+export const Layout: FC<PropsWithChildren> = props => {
+    const { loginUserInfo }: CustomerPortalAppContextModel = useContext(CustomerPortalAppContext)
+    const userRoles = useMemo(() => loginUserInfo?.roles || [], [loginUserInfo?.roles])
+    const isUnprivilegedUser = useMemo(() => {
+        if (!loginUserInfo) return true
+
+        return !userRoles.some(role => PRIVILEGED_ROLES.includes(role))
+    }, [loginUserInfo, userRoles])
+
+    if (isUnprivilegedUser) {
+        return (
+            <RestrictedPage />
+        )
+    }
+
+    return (
+        <>
+            <NavTabs />
+            <ContentLayout
+                innerClass={styles.contantentLayoutInner}
+                outerClass={styles.contentLayoutOuter}
+            >
+                <div className={styles.layout}>
+                    <div className={styles.main}>{props.children}</div>
+                </div>
+            </ContentLayout>
+        </>
+    )
+}
 
 export default Layout
