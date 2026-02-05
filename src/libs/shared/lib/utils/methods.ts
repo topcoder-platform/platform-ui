986a054364a519cc3ae91dd19f532beeb681d055
himaniraghav3 <himaniraghav0311@gmail.com>
2026-02-05 16:23:42 +0530

    clean up unused function

--- src/libs/shared/lib/utils/methods.ts
+++
@@ -1,8 +0,0 @@
-export function upsertTrait<T extends object>(
-    key: keyof T | string,
-    value: any,
-    data: T[] = [],
-): T[] {
-    const filtered = data.filter(item => !(key in item))
-    return [...filtered, { [key]: value } as T]
-}
