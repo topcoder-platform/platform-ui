aa72957b1fbd96fd3dbe837aca308299c7596423
Vasilica Olariu <olariu.vasilica@gmail.com>
2026-02-13 11:28:36 +0200

    lint

--- src/apps/profiles/src/member-profile/phones/Phones.tsx
+++ src/apps/profiles/src/member-profile/phones/Phones.tsx
@@ -50,9 +50,10 @@ const Phones: FC<PhonesProps> = (props: PhonesProps) => {
             props.refreshProfile(props.profile.handle)
         }, 1000)
     }
+
     // Don't render anything if user cannot edit AND cannot see any contact info
     const hasContactInfo = props.profile?.email || phones.length > 0
-    if ( !canEdit && (!canSeeEmailValue || !hasContactInfo)) {
+    if (!canEdit && (!canSeeEmailValue || !hasContactInfo)) {
         return <></>
     }
 
