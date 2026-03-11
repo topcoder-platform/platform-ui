53181dba180c9eb04fca8b9c9591333590c35441
Hentry Martin <hentrymartin.p@gmail.com>
2026-03-11 12:44:06 +0100

    fix: lint

--- src/apps/review/src/lib/components/ChallengeLinks/ChallengeLinks.tsx
+++ src/apps/review/src/lib/components/ChallengeLinks/ChallengeLinks.tsx
@@ -41,8 +41,8 @@ export const ChallengeLinks: FC<Props> = (props: Props) => {
     // Payments button visibility: only copilots and admins
     const canShowPaymentsButton = useMemo(
         () => (
-            [ADMIN, COPILOT].includes(actionChallengeRole as any) ||
-            myResources.some(resource => resource.roleName?.toLowerCase() === 'copilot')
+            [ADMIN, COPILOT].includes(actionChallengeRole as any)
+            || myResources.some(resource => resource.roleName?.toLowerCase() === 'copilot')
         ),
         [actionChallengeRole],
     )
