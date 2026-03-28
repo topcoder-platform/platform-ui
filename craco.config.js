4a345e2673cd5560729e21c3e94a634823ef0880
Adam Meikle <adammeikle@Adams-MacBook-Air.local>
2026-03-27 23:57:34 -0400

    updated craco and routes and verified build

--- craco.config.js
+++ craco.config.js
@@ -43,7 +43,8 @@ module.exports = {
                 target: 'http://localhost:8081',
                 changeOrigin: true,
             },    
-        },
+        }
+    },
 
     webpack: {
         alias: {
