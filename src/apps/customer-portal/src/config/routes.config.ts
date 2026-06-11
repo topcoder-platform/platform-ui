97d40e8e2af5b1e82b5da1b43304816764186db7
Harshit Chudasama <harshitchudasama373@gmail.com>
2026-06-11 17:45:14 +0530

    Clean up talent-search unused code and linting errors

--- src/apps/customer-portal/src/config/routes.config.ts
+++ src/apps/customer-portal/src/config/routes.config.ts
@@ -7,3 +7,5 @@ export const rootRoute: string
     = EnvironmentConfig.SUBDOMAIN === AppSubdomain.customer
         ? ''
         : `/${AppSubdomain.customer}`
+
+export const profileCompletionRouteId = 'profile-completion'
