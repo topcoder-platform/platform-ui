824dd183999aac97344848bc0b5f2683bb639725
Vasilica Olariu <olariu.vasilica@gmail.com>
2026-08-18 14:57:00 +0300

    PM-5370 - clear out old code

---
+++ src/apps/campus/src/campus.routes.spec.tsx
@@ -0,0 +1,56 @@
+/* eslint-disable import/no-extraneous-dependencies, ordered-imports/ordered-imports */
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter, Route, Routes, useLocation } from 'react-router-dom'
+
+import { campusRoutes, rootRoute } from './campus.routes'
+
+jest.mock('~/config', () => ({
+    EnvironmentConfig: {
+        SUBDOMAIN: 'campus',
+    },
+}), {
+    virtual: true,
+})
+
+jest.mock('~/config/constants', () => ({
+    AppSubdomain: {
+        campus: 'campus',
+    },
+    ToolTitle: {
+        campus: 'Campus',
+    },
+}), {
+    virtual: true,
+})
+
+jest.mock('~/libs/core', () => ({
+    lazyLoad: () => (): undefined => undefined,
+}), {
+    virtual: true,
+})
+
+const LocationViewer = (): JSX.Element => {
+    const location = useLocation()
+
+    return <div data-testid='location-pathname'>{location.pathname}</div>
+}
+
+describe('campus routes', () => {
+    it('redirects the campus root to /mecw when groupName is missing', async () => {
+        const campusAppRoute = campusRoutes[0]
+        const campusChildRoutes = campusAppRoute.children || []
+        const fallbackRoute = campusChildRoutes.find(route => route.route === '')
+
+        render(
+            <MemoryRouter initialEntries={[rootRoute || '/']}> 
+                <Routes>
+                    <Route element={<LocationViewer />} path={`${rootRoute}/mecw`} />
+                    <Route element={fallbackRoute?.element} path={rootRoute || '/'} />
+                </Routes>
+            </MemoryRouter>,
+        )
+
+        expect((await screen.findByTestId('location-pathname')).textContent)
+            .toBe('/mecw')
+    })
+})
--- src/apps/campus/src/campus.routes.tsx
+++ src/apps/campus/src/campus.routes.tsx
@@ -1,11 +1,8 @@
 import { lazyLoad, LazyLoadedComponent, PlatformRoute } from '~/libs/core'
+import { Navigate } from 'react-router-dom'
 import { AppSubdomain, EnvironmentConfig, ToolTitle } from '~/config'
 
 const CampusApp: LazyLoadedComponent = lazyLoad(() => import('./CampusApp'))
-const CampusHomePage: LazyLoadedComponent = lazyLoad(
-    () => import('./home'),
-    'CampusHomePage',
-)
 const CampusLeaderboardPage: LazyLoadedComponent = lazyLoad(
     () => import('./pages/leaderboard'),
     'CampusLeaderboardPage',
@@ -22,9 +19,7 @@ export const campusRoutes: ReadonlyArray<PlatformRoute> = [
         authRequired: true,
         children: [
             {
-                children: [],
-                element: <CampusHomePage />,
-                id: 'Campus Home',
+                element: <Navigate replace to={`${rootRoute}/mecw`} />,
                 route: '',
             },
             {
