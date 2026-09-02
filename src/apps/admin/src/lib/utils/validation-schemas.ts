59291545936f565ebad1bd0f137a66518405b763
Vasilica Olariu <olariu.vasilica@gmail.com>
2026-09-02 10:46:09 +0300

    PM-5145 - Fix default reviewers in sys admin

--- src/apps/admin/src/lib/utils/validation-schemas.ts
+++ src/apps/admin/src/lib/utils/validation-schemas.ts
@@ -20,8 +20,14 @@ export const formSearchDefaultReviewersSchema: Yup.ObjectSchema<FormSearchDefaul
 
 export const formAddDefaultReviewerSchema: Yup.ObjectSchema<FormAddDefaultReviewer>
     = Yup.object({
-        aiWorkflowId: Yup.string()
-            .optional(),
+        aiConfigTemplateId: Yup.string()
+            .optional()
+            .when('isMemberReview', {
+                is: true,
+                otherwise: schema => schema.optional(),
+                then: schema => schema
+                    .required('AI Review Template is required'),
+            }),
         baseCoefficient: Yup.number()
             .optional()
             .min(0, 'Must be non-negative')
